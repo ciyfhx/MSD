86639c5a7a51668bbe1ea97d62d7d005a622aad0
ciyfhx <pehziheng@gmail.com>
2019-01-24 10:00:52 +0800

    Update 4.3

---
+++ 4.3/4.3/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  4.3
+//
+//  Created by Student1 on 2019-01-24.
+//  Copyright © 2019 ziheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
