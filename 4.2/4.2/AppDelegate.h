c7fdaeb5ac063f1cdb3901aff1a07748f0262bb1
ciyfhx <pehziheng@gmail.com>
2019-01-24 08:27:45 +0800

    Update 4.1

---
+++ 4.2/4.2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  4.2
+//
+//  Created by Student1 on 2019-01-24.
+//  Copyright © 2019 ziheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
